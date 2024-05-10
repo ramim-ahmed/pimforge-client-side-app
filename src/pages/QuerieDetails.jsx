3cd61354b04054e6c3aadd02035b0eca38f4cacb
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-05-11 03:36:25 +0600

    [DONE] alternate-recommendation-product

--- src/pages/QuerieDetails.jsx
+++ src/pages/QuerieDetails.jsx
@@ -1,16 +1,40 @@
 import { Button } from "@/components/ui/button";
 import useAxios from "@/hooks/useAxios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { FaPlus } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
 import { BarLoader } from "react-spinners";
-import { FaPlus } from "react-icons/fa6";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import useAuth from "@/hooks/useAuth";
+import { useRef } from "react";
+import toast from "react-hot-toast";
+
 export default function QuerieDetails() {
+  const recommendationTitleRef = useRef();
+  const prductNameRef = useRef();
+  const productImageRef = useRef();
+  const productReasonRef = useRef();
   const { id } = useParams();
+  const { authUser } = useAuth();
   const axiosIntance = useAxios();
   const { data, isLoading } = useQuery({
     queryKey: [id],
     queryFn: async () => await axiosIntance.get(`/queries/${id}`),
   });
+  const { mutateAsync: addRecommendation } = useMutation({
+    mutationFn: async (data) =>
+      await axiosIntance.post("/recommendations/create-new", data),
+  });
+
   if (isLoading) {
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -19,6 +43,7 @@ export default function QuerieDetails() {
     );
   }
   const {
+    _id,
     photo,
     name,
     brand,
@@ -29,6 +54,32 @@ export default function QuerieDetails() {
     user,
   } = data?.data?.data || {};
   const { name: userName, image } = user || {};
+  const handleRecommendationProduct = async (e) => {
+    e.preventDefault();
+    const data = {
+      queryUser: {
+        ...user,
+      },
+      recommendationUser: {
+        name: authUser?.displayName,
+        email: authUser?.email,
+        image: authUser?.photoURL,
+      },
+      querieId: _id,
+      productName: name,
+      querieTitle: queryTitle,
+      title: recommendationTitleRef.current.value,
+      name: prductNameRef.current.value,
+      image: productImageRef.current.value,
+      reason: productReasonRef.current.value,
+    };
+    try {
+      await addRecommendation(data);
+      toast.success("Recommendation Added Successfully!!!");
+    } catch (error) {
+      toast.error("Recommendation Added Failed!!");
+    }
+  };
   return (
     <div className="my-10 px-3">
       <div className="max-w-6xl grid grid-cols-12 mx-auto p-5 bg-gray-50">
@@ -67,10 +118,59 @@ export default function QuerieDetails() {
             </div>
           </div>
           <div className="mt-4">
-            <Button className="bg-themeColor space-x-2">
-              <FaPlus className="w-6 h-6 text-white" />
-              <span>Add Alternate Recommendation Product</span>
-            </Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="bg-themeColor space-x-2">
+                  <FaPlus className="w-6 h-6 text-white" />
+                  <span>Add Alternate Recommendation Product</span>
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-3xl">
+                <DialogHeader className="px-4">
+                  <DialogTitle className="text-center">
+                    Add Your Alternative Product Info.
+                  </DialogTitle>
+                </DialogHeader>
+                <div>
+                  <form
+                    onSubmit={handleRecommendationProduct}
+                    className="p-4 space-y-4"
+                  >
+                    <div>
+                      <Input
+                        ref={recommendationTitleRef}
+                        placeholder="Recommendation Title"
+                      />
+                    </div>
+                    <div>
+                      <Input
+                        ref={prductNameRef}
+                        placeholder="Recommendation Protuct Name"
+                      />
+                    </div>
+                    <div>
+                      <Input
+                        ref={productImageRef}
+                        placeholder="Recommendation Product ImageURL"
+                      />
+                    </div>
+                    <div>
+                      <Input
+                        ref={productReasonRef}
+                        placeholder="Recommendation Reason"
+                      />
+                    </div>
+                    <div className="mt-4">
+                      <DialogClose asChild>
+                        <Button type="submit" className="w-full">
+                          Add Recommendation
+                        </Button>
+                      </DialogClose>
+                    </div>
+                  </form>
+                </div>
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
       </div>
